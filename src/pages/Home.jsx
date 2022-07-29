f409eb4b0e34ba36d25733cb84c33163d3bdec83
Kreisler Ramirez <itskreisler@outlook.com>
2022-07-29 15:23:58 -0500

    fix

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Hello word</div>
+  )
+}
+
+export default Home
--- src/pages/Urls.jsx
+++ src/pages/Urls.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
+import Home from './Home'
 const pathTitle = ([path, title]) => { return { path, title } }
 const PATHS = {
   URL_DEFAULT: pathTitle(['/', 'Inicio']),
-  URL_H: pathTitle(['/home', 'Hogar'])
+  URL_H: pathTitle(['/skllis', 'Skllis'])
 }
 const appPages = [
   {
     title: PATHS.URL_DEFAULT.title,
     url: PATHS.URL_DEFAULT.path,
-    Component: () => {
-      return <>Inicio</>
-    }
+    Component: <Home/>
   },
   {
     title: PATHS.URL_H.title,
     url: PATHS.URL_H.path,
     Component: () => {
-      return <>HOGAR</>
+      return <>Habilidades</>
     }
   }]
 export { PATHS, appPages }
